3cf2fbc02598d4a280494f716d5f12ff31c78b0a
MerlYanez13 <71855710+MerlYanez13@users.noreply.github.com>
2020-10-30 21:49:24 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Intro-to-Matter.js
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world, ground, ball;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  var options = {
+    isStatic : true,
+    restitution :1 
+  }
+  ground = Bodies.rectangle(400,390, 800,20,options);
+  World.add(world, ground);
+  console.log(ground);
+
+  var balloptions= {
+    restitution:0.5,
+    isStatic : false
+  }
+
+  ball=Bodies.circle(400,100,40, balloptions);
+  World.add(world, ball);
+  
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+  fill ("green");
+  rectMode(CENTER);
+  rect (ground.position.x,ground.position.y,800,20) ;
+  fill ("red");
+  ellipseMode(RADIUS);
+  ellipse (ball.position.x, ball.position.y, 40,40);
+}
